963e318cc045f196a4f27332ce95c21debcfa39e
jahausma <80059739+jahausma@users.noreply.github.com>
2026-06-10 00:08:00 -0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::unordered_map<int, int> hashMap;
+
+        for(int i = 0; i < nums.size(); ++i){
+            if(hashMap.contains(nums[i])){
+                return true;
+            }
+            hashMap.insert({nums[i],i});
+        }
+    return false;
+    }
+};
