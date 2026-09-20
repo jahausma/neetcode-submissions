f510c5385cc7ce3d57040ac8363089d8489b89e8
jahausma <80059739+jahausma@users.noreply.github.com>
2026-09-20 17:52:55 -0400

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        res = []
+        nums.sort()
+        numList = []
+
+        # use currSum to store sum value as using sum() increases
+        # time complexity from O(2^n) to O(k*2^n)
+
+        def dfs(i, currSum):
+            if currSum == target:
+                res.append(numList.copy())
+                return
+            
+            for j in range(i,len(nums)):
+                if currSum + nums[j] > target:
+                    return
+                numList.append(nums[j])
+                dfs(j, currSum + nums[j])
+                numList.pop()
+            
+        dfs(0,0)
+        return res
+
+            
